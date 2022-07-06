e2c880d5e53d5e8e9295df1d71e33082efda6d32
Yuval <yuvalvarshevsky@gmail.com>
2022-07-07 02:40:23 +0300

    better test structure

---
+++ tests/integration/server.test.ts
@@ -0,0 +1,27 @@
+import { build } from "../../src/app";
+import { test } from "tap";
+
+test("Server integration tests", () => {
+  test("User registration test", async t => {
+    const app = build();
+
+    const res = await app.inject({
+      method: "POST",
+      url: "/api/users",
+      payload: {
+        name: "greg",
+        email: "ar@a.com",
+        age: 22,
+        password: "12345",
+      },
+    });
+    const parsedResponse = JSON.parse(res.payload);
+
+    t.equal(res.statusCode, 201, "returns a status code of 201");
+    t.hasStrict(parsedResponse, {
+      name: "greg",
+      email: "ar@a.com",
+      age: 22,
+    });
+  });
+});
---
+++ tests/unit/server.test.ts
@@ -0,0 +1,8 @@
+import { test } from "tap";
+
+test("Server unit Tests", () => {
+  test("placeholder", async t => {
+    const k = 2;
+    t.equal(k, 2);
+  });
+});
