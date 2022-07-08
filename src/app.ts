567460309881fa7885d6a2146c13021cda4fea0d
Yuval <yuvalvarshevsky@gmail.com>
2022-07-08 15:15:16 +0300

    test fix

--- src/app.ts
+++ src/app.ts
@@ -6,20 +6,18 @@ import itemRouter from "./routes/item.router";
 import fastifySwagger from "@fastify/swagger";
 import { swagOptions } from "./utils/swagger";
 import fastifyCors from "@fastify/cors";
-import { getSchemas } from "./schemas";
+import { addSchemas } from "./schemas";
 
 export const build = (opts: FastifyServerOptions = {}) => {
   const app = fastify(opts).withTypeProvider<TypeBoxTypeProvider>();
 
-  for (const schema of getSchemas()) {
-    app.addSchema(schema);
-  }
+  addSchemas(app);
 
   app.decorateReply("user", null);
   app.decorate("auth", authPreHandler);
   app.decorate("authAdmin", authAdmin);
 
-  app.register(fastifyCors);
+  app.register(fastifyCors, { origin: "*" });
   app.register(fastifySwagger, swagOptions);
   app.register(userRouter, { prefix: "api/users" });
   app.register(itemRouter, { prefix: "api/items" });
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,12 +1,22 @@
-import { FastifyInstance } from "fastify";
+import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import { FastifyInstance, FastifyLoggerInstance } from "fastify";
+import { Server, IncomingMessage, ServerResponse } from "http";
 import { itemSchemaArray } from "./item.schema";
 import { userSchemaArray } from "./user.schema";
 
-export const getSchemas = () => {
+const getSchemas = () => {
   return [...itemSchemaArray, ...userSchemaArray];
 };
 
-export const addSchemas = (app: FastifyInstance) => {
+export const addSchemas = (
+  app: FastifyInstance<
+    Server,
+    IncomingMessage,
+    ServerResponse,
+    FastifyLoggerInstance,
+    TypeBoxTypeProvider
+  >
+) => {
   for (const schema of getSchemas()) {
     app.addSchema(schema);
   }
